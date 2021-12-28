4f4b701372ee3f7b1d8039cc30eafb6b3892335a
aleksas <aasmc@yandex.ru>
2021-12-28 14:50:05 +0300

    Implement RedditApi

---
+++ app/src/main/java/ru/aasmc/redditpager/api/RedditApi.kt
@@ -0,0 +1,51 @@
+package ru.aasmc.redditpager.api
+
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import ru.aasmc.redditpager.model.RedditPost
+
+interface RedditApi {
+
+    @GET("/r/{subreddit}/hot.json")
+    suspend fun getTop(
+        @Path("subreddit") subreddit: String,
+        @Query("limit") limit: Int,
+        @Query("after") after: String? = null,
+        @Query("before") before: String? = null
+    ): ListingResponse
+
+    class ListingResponse(val data: ListingData)
+
+    class ListingData(
+        val children: List<RedditChildrenResponse>,
+        val after: String?,
+        val before: String?
+    )
+
+    data class RedditChildrenResponse(val data: RedditPost)
+
+    companion object {
+        private const val BASE_URL = "https://www.reddit.com"
+
+        fun create(): RedditApi {
+            val logger = HttpLoggingInterceptor { Log.d("API", it) }
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RedditApi::class.java)
+        }
+    }
+
+}
