c56a243e054bf9d7ef69ababffc7a1cd47bdef1f
Alex Alexeyev <aasmc@yandex.ru>
2021-12-29 20:28:51 +0300

    Add click listeners in MainActivity to launch RedditActivity

--- app/src/main/java/ru/aasmc/redditpager/MainActivity.kt
+++ app/src/main/java/ru/aasmc/redditpager/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.aasmc.redditpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.aasmc.redditpager.databinding.ActivityMainBinding
+import ru.aasmc.redditpager.repo.RedditPostRepository
+import ru.aasmc.redditpager.ui.RedditActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.withDatabase.setOnClickListener {
+            show(RedditPostRepository.Type.DB)
+        }
+        binding.networkOnly.setOnClickListener {
+            show(RedditPostRepository.Type.IN_MEMORY_BY_ITEM)
+        }
+        binding.networkOnlyWithPageKeys.setOnClickListener {
+            show(RedditPostRepository.Type.IN_MEMORY_BY_PAGE)
+        }
+    }
+
+    private fun show(type: RedditPostRepository.Type) {
+        val intent = RedditActivity.intentFor(this, type)
+        startActivity(intent)
     }
 }
